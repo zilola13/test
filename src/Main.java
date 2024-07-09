8fbd15bf549b8c539b4ec63c17b7dfb77aaa2438
sprd0x <saparniyazov.aziz@gmail.com>
2024-07-09 16:24:43 +0500

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 }
+
+class User {
+    private String username;
+    private String password;
+    private List<User> users = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void register(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        users.add(user);
+    }
+        }

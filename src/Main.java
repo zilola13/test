fe67dfb697ce522a26eb940428e57efddaf8cc11
sprd0x <saparniyazov.aziz@gmail.com>
2024-07-09 16:30:07 +0500

    BIG FIX

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-}
+    private static class User {
+        private String username;
+        private String password;
 
-class User {
-    private String username;
-    private String password;
-    private List<User> users = new ArrayList<>();
+        public String getUsername() {
+            return username;
+        }
 
-    public String getUsername() {
-        return username;
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    private static List<User> users = new ArrayList<>();
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void register(String username, String password) {
+    public static void register(String username, String password) {
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
         users.add(user);
     }
-        }
+
+    public static void main(String[] args) {
+
+    }
+}
